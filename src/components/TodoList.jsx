4c10f148e91779d5ae4eeb59ce961febdff6d072
EVAN <enriquevanegas@outlook.com>
2023-01-11 19:16:09 -0600

    ToDo Components
    ToDo Comomponents added

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TodoItem } from './TodoItem';
+
+export function TodoList({ todos, toggleTodo }) {
+  return (
+    <ul>
+      {
+        //Los itemes de una lista requieren un 'key' unico
+        todos.map((todo, index) => <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />)
+      }
+    </ul>
+  )
+}
