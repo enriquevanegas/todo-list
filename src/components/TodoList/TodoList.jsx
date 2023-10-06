75f6688a62a0de9fd91dc73699b74a4bcfa7757c
EVAN <enriquevanegas@outlook.com>
2023-10-05 21:59:33 -0600

    CONDITIONAL RENDERING ADDED
    Conditional rendering added to component.

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { TodoItem } from '../TodoItem/TodoItem';
+import './TodoList.css'
 
 export function TodoList({ todos, toggleTodo }) {
   return (
-    <ul>
-      {
-        //Los itemes de una lista requieren un 'key' unico
-        todos.map((todo, index) => <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />)
-      }
-    </ul>
+    //Los itemes de una lista requieren un 'key' unico
+    todos.length != 0 ?
+      <ul className="todo-list">{todos.map((todo, index) => <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />)}</ul>
+      : <p className="no-items">NO TASK</p>
   )
 }
